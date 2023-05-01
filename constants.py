44b4e10b24e0e66eaa3770ed618680ef525a98d5
sandbornm <michael.sandborn@vanderbilt.edu>
2023-05-01 14:34:30 -0500

    input txt validation

--- constants.py
+++ constants.py
@@ -4,6 +4,7 @@ PROBLEMS = ["bnh", "osy", "tnk"] # das-cmop
 SOLVERS = ["", "pymoo", "constraint_prog", "z3", "GA_simple", "scipy"]
 PAGES = ["Welcome", "Problem Overview", "Generate Problems", "Solve Problems", "View Results"]
 
-PROBLEMS_ABSPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "symbench-dataset", "symbench_dataset", "problems")
+#PROBLEMS_ABSPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "symbench-dataset", "symbench_dataset", "problems")
 RESULTS_ABSPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "symbench-studio-data", "results")
-PROBLEM_SPECS_ABSPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "symbench-studio-data", "problems")
+PROBLEM_INPUTS_ABSPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "symbench-studio-data", "default-problems")
+USER_PROBLEM_INPUTS = os.path.join(os.path.dirname(os.path.abspath(__file__)), "symbench-studio-data", "user-problems")
--- spa.py
+++ spa.py
@@ -1,56 +1,83 @@
 import streamlit as st
 import os
 import shutil
-from constants import PROBLEMS_ABSPATH, SOLVERS
+from constants import PROBLEM_INPUTS_ABSPATH, USER_PROBLEM_INPUTS, SOLVERS
+
+import re
+import subprocess
 
 
-# Handle button actions
-tmp_directory = "tmp"
-if not os.path.exists(tmp_directory):
-    os.mkdir(tmp_directory)
-
+def solve_problem(problem_name, solver_name):
+    if st.session_state.user_modified:
+        input_file_path = os.path.join(USER_PROBLEM_INPUTS, problem_name, "user_input.txt")
+    else:
+        input_file_path = os.path.join(PROBLEM_INPUTS_ABSPATH, problem_name, "input.txt")
+    st.write(f"solving problem with input file {input_file_path} using solver {solver_name}")
 
 def load_input_file(problem_name):
-    file_path = os.path.join(PROBLEMS_ABSPATH, problem_name, "input.txt")
+    file_path = os.path.join(PROBLEM_INPUTS_ABSPATH, problem_name, "input.txt")
     with open(file_path, "r") as f:
         content = f.read()
     return content
 
 
-def save_tmp_input_file(problem_name, content):
-    print("saving tmp file")
-    tmp_file_path = os.path.join(tmp_directory, f"{problem_name}_input.txt")
+def save_user_modified_input_file(problem_name, content):
+    print("saving user modified file")
+    tmp_file_path = os.path.join(USER_PROBLEM_INPUTS, problem_name, "user_input.txt")
     with open(tmp_file_path, "w") as f:
         f.write(content)
+    st.session_state.from_user = True
+
 
 def on_text_area_change():
-    save_tmp_input_file(problem_name, st.session_state.input_text_area)
+    result = validate_user_input_file()
+    if result is not True:
+        st.error(result)
+    else:
+        st.success("Valid input file")
+        save_user_modified_input_file(problem_name, st.session_state.input_text_area)
+        st.session_state.user_modified = True
+        st.write("Saved user modified file")
 
+def validate_user_input_file():
+    
+    input_str = st.session_state.input_text_area.strip().split("\n")
+    line_number = 1
+    
+    variable_pattern = re.compile(r'^variable\s+\w+\s+-?\d+(\.\d+)?\s+-?\d+(\.\d+)?$')
+    constraint_pattern = re.compile(r'^constraint\s+\w+\s+(leq|geq)\(.+\,-?\d+(\.\d+)?\)$')
+    projection_pattern = re.compile(r'^projection\s+\w+\s+.+$')
+    optimize_pattern = re.compile(r'^(minimize|maximize)\s+\w+$')
+
+    for line in input_str:
+        if not (variable_pattern.match(line) or constraint_pattern.match(line) 
+                or projection_pattern.match(line) or optimize_pattern.match(line)):
+            return f"Invalid format at line {line_number}: {line}"
+        line_number += 1
+    
+    return True
+
+def reset_text_area():
+    st.session_state.input_text_area = load_input_file(problem_name)
+    st.session_state.user_modified = False
+
+
+
+st.session_state.from_user = False  # assume original problem will be solved
 
 main_container = st.container()
 main_container.title("Optimization Problem Solver")
-problem_name = main_container.selectbox("Select a problem:", os.listdir(PROBLEMS_ABSPATH), on_change=st.experimental_rerun)
+problem_name = main_container.selectbox("Select a problem:", os.listdir(PROBLEM_INPUTS_ABSPATH))
 input_file_content = load_input_file(problem_name)
 
 # Initialize the session state for the input_text_area
 if 'input_text_area' not in st.session_state:
     st.session_state.input_text_area = load_input_file(problem_name)
+    st.session_state.user_modified = False
 
-# Add the text area with the session state value
-input_text_area = st.text_area("Edit the input file:", st.session_state.input_text_area, key="input_text_area", on_change=on_text_area_change)
+input_text_area = st.text_area("Edit the input file:", st.session_state.input_text_area, height=500, key="input_text_area", on_change=on_text_area_change)
+reset_button = main_container.button("Reset to Original", on_click=lambda: st.session_state.pop("input_text_area", None))
+validate_button = main_container.button("Validate Input File", on_click=validate_user_input_file)
 
-# Add action buttons
-reset_button = main_container.button("Reset to Original")
-
-if reset_button:
-    print("resetting")
-    st.session_state.input_text_area = load_input_file(problem_name)
-    st.experimental_rerun()
-
-# # Add a container for the solver
-# solver_container = st.container()
-
-# # Add solver selection and execution
-# with solver_container.expander("Select Solver"):
-#     solver = st.selectbox("Choose a solver:", SOLVERS, index=0)
-#     run_solver_button = st.button("Run Solver")
+solver_selection = main_container.selectbox("Select a solver:", SOLVERS)
+solve_button = main_container.button("Solve", on_click=lambda: solve_problem(problem_name, solver_selection))
