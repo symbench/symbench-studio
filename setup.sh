aecfc5843ea1c27bdff84e8ed996b35ba0bb124e
sandbornm <michael.sandborn@vanderbilt.edu>
2023-05-19 16:36:41 -0500

    rewrite main merge and update reqs

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 
 # ensure submodules are updated
 echo "Updating submodules..."
-git submodule update --init --recursive
+git submodule update --init
 
 # ensure submodules are installed as packages
 echo "Installing requirements..."
 cd constraint-prog && pip install -e . && cd ..
 cd symbench-dataset && pip install -e . && cd ..
+cd cp-problems-generator && pip install -e . && cd ..
 
 # install requirements
 pip install -r requirements.txt
